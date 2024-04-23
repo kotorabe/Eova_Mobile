c9380bfc0b02a42b314adaf7f9b4835b98ab059f
kotorabe <kotorajao9@gmail.com>
2024-04-23 10:51:03 +0300

    Initialize 0.0.1

--- Eova/helpers/api_url.js
+++
@@ -1,4 +0,0 @@
-// export const api_url =  "http://192.168.88.146:8000/api";
-export const api_url =  "http://192.168.0.103:8000/api";
-// export const api_url =  "http://192.168.0.108:8000/api";
-// export const api_url =  "http://192.168.0.104:8000/api";
